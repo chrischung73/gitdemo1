7456f00f7c7403e9f84d395834b69416017b872b
mark <mark@hotmail.com>
2012-08-13 19:59:45 +0800

    add an activity and layout xml
    Signed-off-by: mark <mark@hotmail.com>

---
+++ gitdemo1/src/www/uuu/git/Order.java
@@ -0,0 +1,14 @@
+package www.uuu.git;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Order extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {		
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.order);
+	}
+
+}
