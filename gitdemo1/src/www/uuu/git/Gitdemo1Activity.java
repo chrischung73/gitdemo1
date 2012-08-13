9478124e22920bd28692755e9b413fef7fd8a5e6
mark <mark@hotmail.com>
2012-08-13 19:46:58 +0800

    Initial check-in

---
+++ gitdemo1/src/www/uuu/git/Gitdemo1Activity.java
@@ -0,0 +1,13 @@
+package www.uuu.git;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Gitdemo1Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
