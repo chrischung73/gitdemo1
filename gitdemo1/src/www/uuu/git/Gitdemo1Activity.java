66c5a34db5d3a7030daba2eaf9f78d37c1f4616c
mark <mark@hotmail.com>
2012-08-13 20:12:49 +0800

    modify main add button lunch doOrder()
    Signed-off-by: mark <mark@hotmail.com>

--- gitdemo1/src/www/uuu/git/Gitdemo1Activity.java
+++ gitdemo1/src/www/uuu/git/Gitdemo1Activity.java
@@ -1,7 +1,9 @@
 package www.uuu.git;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Gitdemo1Activity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +11,11 @@ public class Gitdemo1Activity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    public void doOrder(View view)
+    {
+    	Intent intent = new Intent();
+    	intent.setClass(this, Order.class);
+    	startActivity(intent);
     }
 }
